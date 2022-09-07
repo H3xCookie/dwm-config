becb2b55a5ddbb1428612a1dafcd85087b522c3f
h3x <h3x@localhost.localdomain>
2022-09-07 11:56:05 +0300

    Super + Shift + s - Spawn flameshot gui

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
+# REQUIREMENTS
+## flameshot - Screenshot utility
+
 # KEYBINDS
 Super - Windows Key
 
-| Bind          | Action        |
+| Bind | Action |
 | ------------- | ------------- |
-| Super + T     | Spawn st      |
-| Super + Space | Spawn dmenu   |
-| Super + F     | Spawn firefox |
+| Super + T | Spawn st |
+| Super + Space | Spawn dmenu |
+| Super + F | Spawn firefox |
+| Super + Shift + s | Spawn flameshot gui |
--- config.h
+++ config.h
@@ -58,13 +58,15 @@ static const Layout layouts[] = {
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *firefoxcmd[] = {"firefox"};
+static const char *firefoxcmd[] = {"firefox", NULL};
+static const char *flameshotcmd[] = {"flameshot", "gui"};
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_t,      spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_f,      spawn,          {.v = firefoxcmd } },	
+	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = flameshotcmd } },	
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
